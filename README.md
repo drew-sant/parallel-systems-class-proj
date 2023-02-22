071159111f81fa7b9cdf4cf3c6f2a1696bb1e67a
Drew S <sandrewg@byui.edu>
2023-02-22 06:42:51 +0000

    first commit
    The basics for outputting html 'Hello World' to an https request to the host hosting the application.

---
+++ README.md
@@ -0,0 +1,17 @@
+hello
+=====
+
+An OTP application
+
+This is a class project focused on building a Erlang OTP application using
+gen_servers.
+
+--------------------------------------------------------------------------
+Using digital ocean for hosting.
+
+Will need to add a priv/ssl directory and create soft links to ssl key files.
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ src/hello_app.erl
@@ -0,0 +1,36 @@
+%%%-------------------------------------------------------------------
+%% @doc hello public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(hello_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+	Dispatch = cowboy_router:compile([
+        {'_',[
+			{"/", toppage_h, [] }
+		]}
+    ]),
+
+%%    {ok, _} = cowboy:start_clear(my_http_listener,
+%%        [{port, 80}],
+%%        #{env => #{dispatch => Dispatch}}
+
+        PrivDir = code:priv_dir(hello),
+	{ok, _} = cowboy:start_tls(https_listener, [
+						    {port, 443},
+						    {certfile, PrivDir ++ "/ssl/fullchain.pem"},
+						    {keyfile, PrivDir ++ "/ssl/privkey.pem"}
+						   ],
+	#{env => #{dispatch => Dispatch}}
+    ),
+    hello_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
